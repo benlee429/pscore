561e62a4c4458e0c29b10e2b1b9c4e255ef0ab43
benlee429 <blee429@gmail.com>
2015-01-09 14:56:50 -0800

    new pscore repo

---
+++ controllers/mainCtlr.js
@@ -0,0 +1,7 @@
+//main controller
+'use strict';
+
+
+app.controller('mainCtlr', function($scope) {
+  $scope.data = 'test';
+});
---
+++ modules/mainMdle.js
@@ -0,0 +1,5 @@
+//main module
+'use strict';
+
+var app = angular.module('ngApp',['ngRoute','ngResource']); 
+
